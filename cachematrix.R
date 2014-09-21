239353237a06423000974c77c6e4c06bc9b7a6b3
emerton <elizabethmerton@usf-stpt-wifi-224-211-171.laptops.usf.edu>
2014-09-21 14:08:18 -0400

    # Assignment 2: First, cache the inverse of a matrix
    ## This function creates an object that will be able to cache its
    inverse
    
    makeCacheMatrix <- function(x = matrix()) {
    
    }
    i <- NULL
    
    set <- function(matrix){
    		m<<-matrix
    		i<<-NULL
    }
    
    get<- function(){
    	m
    }
    
    setInverse <- function(inverse) {
    	i<<-inverse
    }
    
    list(set=set, get=get,
    	setInverse = setInverse
    	getInverse = getInverse)
    
    ## Then compute the inverse of the special matrix
    
    cacheSolve <- function(x, ...) {
            ## This returns a matrix that is the inverse of 'x'
    }
    m<- x$getInverse()
    
    if( !is.null(m) ){
    	message("getting cached data")
    	return(m)
    }
    data <-x$get()
    
    m <- solve(data) %*% data
    
    x$setInverse(m)
    
    m

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assignment 2: First, cache the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates an object that will be able to cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
+i <- NULL
 
+set <- function(matrix){
+		m<<-matrix
+		i<<-NULL
+}
 
-## Write a short comment describing this function
+get<- function(){
+	m
+}
+
+setInverse <- function(inverse) {
+	i<<-inverse
+}
+
+list(set=set, get=get,
+	setInverse = setInverse
+	getInverse = getInverse)
+
+## Then compute the inverse of the special matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## This returns a matrix that is the inverse of 'x'
 }
+m<- x$getInverse()
+
+if( !is.null(m) ){
+	message("getting cached data")
+	return(m)
+}
+data <-x$get()
+
+m <- solve(data) %*% data
+
+x$setInverse(m)
+
+m
